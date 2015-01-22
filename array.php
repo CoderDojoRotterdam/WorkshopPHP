1e5f916d597f3c5424e8420516b0d1cf5fdc2c51
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2015-01-22 21:28:31 +0100

    Update array.php - vertaling

--- array.php
+++ array.php
@@ -1,119 +1,104 @@
 <?php 
 
 	/*
-
 	-----------------------------------
 	------------- Arrays --------------
 	-----------------------------------
-	
 	-----------------------------------
-	-- Basic Array with strings
+	-- Een array met tekst
 	-----------------------------------
 	
-	-- code
-	
-		$games = array("Mario", "Zelda", "Minecraft");
+	-- Voorbeeld code
+		$spelletjes = array("Mario", "Zelda", "Minecraft");
 		
-		echo $games[0];
+		echo $spelletjes[0];		// Het eerste vakje (Mario)
+		echo $spelletjes[1];		// Het tweede vakje (zelda)
+		echo $spelletjes[2];		// Het derde vakje (Minecraft)
 		
-		echo $games[1];
-		
-		echo $games[2];
-		
-	-- result
+	-- Resultaat:
 	
 		Mario
 		Zelda
 		Minecraft
 	
 	-----------------------------------
-	-- Basic Array with numbers
+	-- Een array met cijfers
 	-----------------------------------
 	
-	-- code
+	-- Voorbeeld code
 	
-		$years = array(1987, 2015, 3000);
+		$jaartallen = array(1987, 2015, 3000);
 		
-		echo "The year is " . $years[0];
+		echo "Het jaar is " . $jaartallen[0];
+		echo "Het jaar is " . $jaartallen[1];
+		echo "Het jaar is " . $jaartallen[2];
 		
-		echo "The year is " . $years[1];
-		
-		echo "The year is " . $years[2];
-		
-	-- result
+	-- Resultaat
 	
-		The year is 1987
-		The year is 2015
-		The year is 3000
+		Het jaar is 1987
+		Het jaar is 2015
+		Het jaar is 3000
+		
+	-------------------------------------------
+	-- Array met tekst en cijfers door elkaar
+	-------------------------------------------
+	
+	-- Voorbeeld code
+	
+		$autos = array("Volkswagen", 2000, "Audi", 3000);
+		
+		echo "De auto " . $autos[0] . " is van het type " . $autos[1];
+		echo "De auto " . $autos[2] . " is van het type " . $autos[3];
+		
+	-- Het resultaat
+	
+		De auto Volkswagen is van het type 2000
+		The car Audi is van het type 3000
 		
 	-----------------------------------
-	-- Array with numbers and strings
+	-- En nu alles door elkaar!
 	-----------------------------------
 	
-	-- code
+	-- Code voorbeeld
+		$namen 		= array("Jon", "Piet");
+		$leeftijden = array(32, 28);
+		
+		echo "Mijn naam is " . $namen[1] . " en ik ben " . $leeftijden[0] . " jaar oud!"
+		
+	-- Resultaat
 	
-		$cars = array("Volgswagen", 2000, "Audi", 3000);
-		
-		echo "The car " . $cars[0] . " is model " . $cars[1];
-		
-		echo "The car " . $cars[2] . " is model " . $cars[3];
-		
-	-- result
-	
-		The car Volgswagen is model 2000
-		The car Audi is model 3000
+		Mijn naam is Piet en ik ben 32 jaar oud!
 		
 	-----------------------------------
-	-- Playing a bit with arrays
+	-- Associative Arrays 
+	-- (INFO: Associatief betekend namen en waarden in plaats van cijfers zoals we hierboven gebruikten)
 	-----------------------------------
 	
-	-- code 
+	-- Code voorbeeld 1
+		
+		$spelletjes = array("nintendo" => "mario", "computer" => "minecraft", "xbox" => 360 );
+		
+		echo $spelletjes["nintendo"];
+		echo $spelletjes["computer"];
+		echo $spelletjes["xbox"];
 	
-		$names = array("Jon", "Piet");
-		
-		$ages = array(32, 28);
-		
-		echo "My name is " . $names[1] . " and I am " . $ages[0] . " years old!"
-		
-	-- result
-	
-		My name is Piet and I am 32 years old!
-		
-	-----------------------------------
-	-- Associative Arrays
-	-----------------------------------
-	
-	-- code method 1
-		
-		$games = array("nintendo" => "mario", "computer" => "minecraft", "xbox" => 360 );
-		
-		echo $games["nintendo"];
-		
-		echo $games["computer"];
-		
-		echo $games["xbox"];
-	
-	-- result
+	-- Resultaat
 	
 		mario
 		minecraft
 		xbox
 		
-	-- code method 2
+	-- Code voorbeeld 2
 	
-		$students["Jon"] = 28;
+		$studenten["Jon"] 	= 28;
+		$studenten["Piet"] 	= "38";
+		$studenten["Kim"] 	= "apple";
 		
-		$students["Piet"] = "38";
-
-		$students["Kim"] = "apple";
+		echo $studenten["Jon"];
+		echo $studenten["Piet"];
+		echo $studenten["Kim"];
 		
-		echo $students["Jon"];
-		
-		echo $students["Piet"];
-		
-		echo $students["Kim"];
-		
-	-- result
+	-- Resultaat
 	
 		28
 		38
@@ -123,29 +108,22 @@
 ?>
 
 <html>
-
-	<head></head>
+	<head>
+		<title>Arrays!</title>
+	</head>
 	<body>
-	
 		<h1>Arrays!</h1>
-		
 		<h3>------------------------------</h3>
 		
 		<p>
 		<?php 
-			
-			//****** Write your code here! ******
-			
-			echo "Your first Array!";
-			
+			// Vanaf hier mag je zelf aan de slag met arrays!
+			echo "Maak een eerste array! Als je het even niet meer weet, kijk dan naar de voorbeelden hierboven!";
 		?>
 		</p>
 		
 		<h3>------------------------------</h3>
-		
-		<a href="index.php">Back to home</a>
-		
+		<a href="index.php">Terug naar het overzicht</a>
 	
 	</body>
-
 </html>
