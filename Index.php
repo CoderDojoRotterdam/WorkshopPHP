c1a1c7e419364e87f275ffcb4f478be8e889d8eb
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2015-01-22 22:02:00 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,45 +1,19 @@
-<?php 
-	
-	
-	/*
-		TODO (Jonathan):
-		
-		Functions
-		Includes and Require
-		Project page for individual students
-		
-		TODO (Anyone):
-		
-		Dutchyfi all the things!
-		
-	*/
-	
-	
-	// Inlcudes are used to include code from to other files. Executes regardless 
-	// include 'file.php';
-	
-	// Requires are used to include code from to to other files. Stops executing if file does not exist.
-	
-	
-?>
 <html>
 
-	<head></head>
+	<head>
+		<title>Workshop PHP</title>
+	</head>
 	<body>
-	
-		<h1>Welcome to the CoderDojo PHP workshop!</h1>
+		<h1>Welkom! Dit is de PHP workshop voor CoderDojo Rotterdam.</h1>
 		
-		<h3>Lessons</h3>
+		<h3>Onderwerpen</h3>
 		<ul>
-			<li><a href="introduction.php">Introduction</a></li>
-			<li><a href="variable.php">Variables</a></li>
+			<li><a href="introduction.php">Introductie</a></li>
+			<li><a href="variable.php">Variabelen</a></li>
 			<li><a href="array.php">Arrays</a></li>
-			<li><a href="loop.php">Loops</a></li>
-			<li><a href="function.php">Functions</a></li>
-			<li><a href="includes.php">Includes and Excludes</a></li>
-			<li><a href="profile.php">Profiles!</a></li>
+			<li><a href="loop.php">Lusjes</a></li>
+			<li><a href="function.php">Functies</a></li>
 		</ul>
 		
 	</body>
-
 </html>
