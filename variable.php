4d3350e50a42f417cfc3ab230f29697781a7bf8a
Christian Vermeulen <info@christianvermeulen.net>
2015-01-24 19:00:31 +0000

    put examples in comment and added assignment

--- variable.php
+++ variable.php
@@ -8,107 +8,89 @@
 		<h3>------------------------------</h3>
 		<?php
 			
-			/**
-			 * Een simpele variabele met tekst (string)
-			 **/
+			/***************************************************
+			
+			///////////////////////
+			// Een simpele variabele met tekst (string)
+			///////////////////////
 			$fruit = "appel";
 			echo $appel;
-		?>
-		<br/>
-		<?php
 			
-			/**
-			 * Variabelen aan elkaar plakken met een punt
-			 **/
+			///////////////////////
+			// Variabelen aan elkaar plakken met een punt
+			///////////////////////
 			$naam 			= "Jon";
 			$achternaam 	= "van Put";
-			$volledige_naam = $name . " " . $achternaam;
+			$hele_naam = $name . " " . $achternaam;
 			echo "Mijn naam  " . $volledige_naam;
 			
-		?>
-		<br/>
-		<?php
-			
-			/**
-			 * Een simpele variabele met getallen
-			 **/
+			///////////////////////
+			// Een simpele variabele met getallen
+			///////////////////////
 			$nummer = 35;
 			echo $number;
-		
-		?>
-		<br/>
-		<?php
+
+			///////////////////////
+			// Nummers aan elkaar plakken
+			///////////////////////
 			
-			/**
-			 * Nummers aan elkaar plakken
-			 **/
 			$nummerA = 1;
 			$nummerB = 5;
 			$nummerResultaat = $nummerA . $nummerB;
 			echo "Het nummer is " . $nummberResultaat;
-		
-		?>
-		<br/>
-		<?php
 			
-			/**
-			 * Rekenen met getallen
-			 **/
+			///////////////////////
+			// Rekenen met getallen
+			///////////////////////
 			
 			// Aftrekken
+			
 			$jaar = 2015;
 			$geboortejaar = 1987;
 			$leeftijd = $jaar - $geboortejaar;
 			echo "Ik ben " . $leeftijd . " jaar oud!";
 		
-		?>
-		<br/>
-		<?php
 		
 			// optellen
+			
 			$appels = 3;
 			$peren = 2;
 			$fruit = $appels + $peren;
 			echo "Ik heb " . $fruit . " stukken fruit";
-		
-		?>
-		<br/>
-		<?php
+
 		
 			// Vermenigvuldigen (keer)
+			
 			$huizen = 9;
 			$gezin = 4;
 			$inwoners = $huizen * $gezin;
 			echo "Er wonen " . $inwoners . " mensen in dit dorp";
-		
-		?>
-		<br/>
-		<?php
+			
 			
 			// Delen
+			
 			$mensen = 6;
 			$koekjes = 24;
 			$ieder = $koekjes / $mensen;
 			echo "iedereen krijgt " . $ieder . " koekjes";
-		
-		?>
-		<br/>
-		<?php
-		
-			/**
-			 * Alles bij elkaar
-			 **/
-			$volledige_naam = "Jon van Put";
-			$leeftijd = 28;
-			$profiel = "Mijn naam is " . $volledige_naam . " en ik ben " . $leeftijd . " jaar oud!";
-			echo $profiel;
+			
+			***************************************************/
 		?>
 		
 		<p>
-		<?php 
-			// Speel hier zelf een beetje met variabelen, als je het even niet meer snapt kun je altijd hierboven even speieken ;)
-			echo "Nu mag je zelf een beetje spelen met variabelen";
-		?>
+			<?php 
+				// OPDRACHT
+				//
+				// Maak een variabele voor de volgende dingen:
+				// - Voornaam
+				// - Achternaam
+				// - Leeftijd
+				// 
+				// Voeg ze nu allemaal samen in 1 zin.
+				// echo "Hoi, mijn naam is [voornaam] [achternaam] en ik ben [leeftijd] jaar oud.";
+				
+				
+			?>
 		</p>
 		
 		<h3>------------------------------</h3>
